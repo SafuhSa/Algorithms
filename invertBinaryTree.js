51a26c4c72747d0ac8b0aafd3993e00a4b98f060
Safuh <Safoh95@windowslive.com>
2019-02-26 16:55:46 -0800

    invetbinary

---
+++ invertBinaryTree.js
@@ -0,0 +1,18 @@
+// AlogExper invertBinaryTree
+
+
+function invertBinaryTree(tree) {
+  h
+  if (!tree) return;
+
+  let right = tree.right;
+  tree.right = tree.left;
+  tree.left = right
+  invertBinaryTree(tree.right)
+  invertBinaryTree(tree.left)
+
+  return tree;
+}
+
+// Do not edit the line below.
+exports.invertBinaryTree = invertBinaryTree;
