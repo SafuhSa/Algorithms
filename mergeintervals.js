ff8b7bdbb10ab2302af2b904e3d02f6ec70c41b5
Safuh <Safoh95@windowslive.com>
2019-05-18 20:19:19 +0100

    mer

--- mergeintervals.js
+++ mergeintervals.js
@@ -1,7 +1,6 @@
 // Given a collection of intervals, merge all overlapping intervals.
 
 //   Example 1:
-
 // Input: [[1, 3], [2, 6], [8, 10], [15, 18]]
 // Output: [[1, 6], [8, 10], [15, 18]]
 // Explanation: Since intervals[1, 3] and[2, 6] overlaps, merge them into[1, 6].
@@ -10,7 +9,6 @@
 // Input: [[1, 4], [4, 5]]
 // Output: [[1, 5]]
 // Explanation: Intervals[1, 4] and[4, 5] are considered overlapping.
-
 var merge = function (intervals) {
   let result = []
   let arr = intervals.slice(0).sort((a, b) => a.start - b.start);
@@ -24,7 +22,6 @@ var merge = function (intervals) {
       result.push(sub);
     }
   }
-
   intervals = result
   return result;
 };
