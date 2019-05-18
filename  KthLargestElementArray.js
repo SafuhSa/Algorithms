e3a71e5e26053a67185e460409854916e11923d7
Safuh <Safoh95@windowslive.com>
2019-05-18 20:19:19 +0100

    kth

---  KthLargestElementArray.js
+++  KthLargestElementArray.js
@@ -1,7 +1,5 @@
 // Find the kth largest element in an unsorted array.Note that it is the kth largest element in the sorted order, not the kth distinct element.
-
 //   Example 1:
-
 // Input: [3, 2, 1, 5, 6, 4] and k = 2
 // Output: 5
 // Example 2:
