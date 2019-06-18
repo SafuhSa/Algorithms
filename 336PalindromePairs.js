b90b4ab11be7f39c1e32092c0a2788eab5cde7ef
Safuh <Safoh95@windowslive.com>
2019-06-17 19:39:44 -0700

    solu

--- 336PalindromePairs.js
+++ 336PalindromePairs.js
@@ -14,9 +14,71 @@
 
 
 // 336. Palindrome Pairs
+var isPalindrome = function(word) {
+  let hlf = Math.floor(word.length/2)
+  for(let i = 0; i < hlf; i++) {
+    if(word[i] !== word[word.length - 1 - i]) return false;
+  }
+  return true;
+};
+
+var Trie = function() {
+  this.next = {};
+  this.belowPal = [];
+  this.wordEnd = false;
+  this.wordIdx = null;
+};
+
+var addWordtoTrie = function (trie, word, idx) {
+  for(let i = word.length -1; i >= 0; i++) {
+    let char = word[i];
+    if(!trie.next[char]) {
+      trie.next[char] = new Trie();
+    }
+    if(isPalindrome(word.substring(0, i+1))) {
+      word.push(idx);
+    }
+    trie = trie.next[char];
+  }
+  trie.wordEnd = true;
+  trie.wordIdx = idx;
+}
+var getPalindromesWord = function(trie, word) {
+  let set = new Set();
+  for(let i =0; i < word.length; i++) {
+    let char = word[i];
+    if(trie.wordEnd) {
+      if(isPalindrome(word.substring(i))) {
+        set.add(trie.wordIdx);
+      }
+    }
+    trie = trie[char];
+  }
+  if(trie.wordEnd) set.add(trie.wordIdx);
+
+  trie.belowPal.forEach(el => {
+    set.add(el);
+  })
+  return set;
+}
 
 var palindromePairs = function (words) {
+  let trie = new Trie();
+  words.forEach((word,idx) => {
+    addWordtoTrie(trie, word, idx);
+  });
 
+  let result =[];
+  words.forEach((word, idx) => {
+    for(let i =0; i < word.length; i++) {
+      let cand = getPalindromesWord(trie, word);
+      cand.forEach(el => {
+        if(el !== idx) result.push([i, el]);
+      })
+    }
+  })
+
+  return result;
 };
 
 let input = ["abcd", "dcba", "lls", "s", "sssll"];
